dfad52f19ba236a94aba5c3d37d1c602c1cb9801
niebl <caro.niebl@uni-muenster.de>
2025-02-07 15:39:41 +0100

    implement time slider

--- static/scripts.js
+++ static/scripts.js
@@ -56,4 +56,62 @@ function findCategories() {
 		alert('Error executing query: ' + (error.response?.data?.error || error.message));
 	})
 
+	return outData
 }
+
+function findDateRange() {
+	const query = "select MIN(measurement_time), MAX(measurement_time) from osem_bike_measurements;"
+	const promise = axios.post('/execute-sql', {query})
+	const outData = promise.then(
+		response => {
+			range = [
+				new Date(response.data[0][0]), 
+				new Date(response.data[0][1])
+			]
+			return range
+		})
+	.catch(error => {
+		console.error(error);
+		alert('Error executing query: ' + (error.response?.data?.error || error.message));
+	})
+
+	return outData
+}
+
+function unixToDatetime(unixString){
+	date = new Date(unixString)
+	//output = date.getDay()+"."+date.getMonth()+"."+date.getFullYear()+" "+ date.getTime()
+	output = date.toISOString()
+	return output
+}
+
+// -------------------------------------------
+// 	MAIN
+// -------------------------------------------
+
+async function main(){
+	const date_range = await findDateRange()
+	const category_list = await findCategories()
+
+	//initiate slider
+	$( function() {
+		$( "#slider-range" ).slider({
+			range: true,
+			min: date_range[0].getTime(),
+			max: date_range[1].getTime(),
+			values: [ date_range[0].getTime(), date_range[1].getTime() ],
+			slide: function( event, ui ) {
+				$( "#amount" ).val( 
+					unixToDatetime(ui.values[ 0 ]) 
+					+ " - " + 
+					unixToDatetime(ui.values[ 1 ]) );
+			}
+		});
+		$( "#amount" ).val( 
+			unixToDatetime($( "#slider-range" ).slider( "values", 0 )) + " - " + 
+			unixToDatetime($( "#slider-range" ).slider( "values", 1 )) 
+			);
+	} );
+}
+
+main()
